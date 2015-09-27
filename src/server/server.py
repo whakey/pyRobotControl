44418ff5026c0088494ca15b2430c0a2220eed89
Ville <ville.tahkavuori@gmail.com>
2015-09-27 19:04:19 +0300

    bla bla

--- src/server/server.py
+++ src/server/server.py
@@ -12,11 +12,10 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(1024).strip()
         while (self.data).decode("utf-8") != "exit":
-            self.data = self.request.recv(1024).strip()
             print("{} wrote:".format(self.client_address[0]))
             print(self.data)
             self.request.sendall(self.data.upper())
-
+            self.data = self.request.recv(1024).strip()
 
 if __name__ == "__main__":
     HOST, PORT = "", int(args.port)
